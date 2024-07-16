8fa94154a486ae9efaf54b49b20c2a0b18a034ce
dogsoft1990 <101684609+dogsoft1990@users.noreply.github.com>
2024-07-16 09:24:21 +0800

    mtgsig
    mtgsig 3.0 美团算法

---
+++ Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace meituan
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.MaxLength = 327670000;
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(602, 196);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 227);
+            textBox2.MaxLength = 327670000;
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.ScrollBars = ScrollBars.Vertical;
+            textBox2.Size = new Size(602, 88);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 335);
+            textBox3.MaxLength = 327670000;
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.ScrollBars = ScrollBars.Vertical;
+            textBox3.Size = new Size(602, 103);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(12, 457);
+            textBox4.MaxLength = 327670000;
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.ScrollBars = ScrollBars.Vertical;
+            textBox4.Size = new Size(608, 249);
+            textBox4.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(656, 22);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "RC4加密";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(656, 71);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "A2MD5";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(656, 139);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 6;
+            button3.Text = "A91";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(656, 227);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 7;
+            button4.Text = "A9-1";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(656, 654);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 8;
+            button5.Text = "A9-5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(656, 391);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 23);
+            button6.TabIndex = 9;
+            button6.Text = "A9-0";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(656, 420);
+            button7.Name = "button7";
+            button7.Size = new Size(75, 23);
+            button7.TabIndex = 10;
+            button7.Text = "A9-2";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(665, 494);
+            button8.Name = "button8";
+            button8.Size = new Size(75, 23);
+            button8.TabIndex = 11;
+            button8.Text = "A9-3";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(656, 601);
+            button9.Name = "button9";
+            button9.Size = new Size(75, 23);
+            button9.TabIndex = 12;
+            button9.Text = "A9-4";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(785, 22);
+            button10.Name = "button10";
+            button10.Size = new Size(70, 28);
+            button10.TabIndex = 13;
+            button10.Text = "CRC3";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(785, 71);
+            button11.Name = "button11";
+            button11.Size = new Size(125, 28);
+            button11.TabIndex = 14;
+            button11.Text = "A9 %3==2算法";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(785, 115);
+            button12.Name = "button12";
+            button12.Size = new Size(125, 28);
+            button12.TabIndex = 15;
+            button12.Text = "A9 %3==0算法";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(656, 694);
+            button13.Name = "button13";
+            button13.Size = new Size(75, 23);
+            button13.TabIndex = 16;
+            button13.Text = "A9-31";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(785, 161);
+            button14.Name = "button14";
+            button14.Size = new Size(125, 28);
+            button14.TabIndex = 17;
+            button14.Text = "A9 %3==1算法";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += button14_Click;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(835, 227);
+            button15.Name = "button15";
+            button15.Size = new Size(75, 23);
+            button15.TabIndex = 18;
+            button15.Text = "xor";
+            button15.UseVisualStyleBackColor = true;
+            button15.Click += button15_Click;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(835, 292);
+            button16.Name = "button16";
+            button16.Size = new Size(75, 23);
+            button16.TabIndex = 19;
+            button16.Text = "xor123";
+            button16.UseVisualStyleBackColor = true;
+            button16.Click += button16_Click;
+            // 
+            // button17
+            // 
+            button17.Location = new Point(959, 227);
+            button17.Name = "button17";
+            button17.Size = new Size(75, 23);
+            button17.TabIndex = 20;
+            button17.Text = "and";
+            button17.UseVisualStyleBackColor = true;
+            button17.Click += button17_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1092, 729);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "美团工具";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+    }
+}
---
+++ Tools.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace meituan
+{
+    internal static class Tools
+    {
+
+
+
+        public static byte[] filldata(byte[] inputdata)
+        {
+            int len = inputdata.Length;
+
+            int mod = len % 0x10;
+
+            int fill = 0x10 - mod;
+
+            PackOperate Operate = new PackOperate();
+
+            Operate.SetBytes(inputdata);
+
+            for (int i = 0; i < fill; i++)
+            {
+
+                Operate.SetByte(fill);
+            }
+
+
+            return Operate.GetAll();
+
+        }
+        public static byte[] AesEncrypt(byte[] src, byte[] key, byte[] iv, string cipher, PaddingMode padding)
+        {
+            byte[] resultArray = new byte[0];
+
+            Aes aes = Aes.Create();
+
+            aes.Key = key;
+
+            switch (cipher)
+            {
+                case "CBC":
+                    resultArray = aes.EncryptCbc(src, iv, padding);
+                    break;
+                case "ECB":
+                    resultArray = aes.EncryptEcb(src, padding);
+
+                    break;
+                case "CFB":
+                    resultArray = aes.EncryptCfb(src, iv, padding);
+                    break;
+            }
+            return resultArray;
+        }
+
+        public static byte[] AesDecrypt(byte[] src, byte[] key, byte[] iv, string cipher, PaddingMode padding)
+        {
+            byte[] resultArray = new byte[0];
+
+            Aes aes = Aes.Create();
+
+            aes.Key = key;
+
+            switch (cipher)
+            {
+
+
+                case "CBC":
+                    resultArray = aes.DecryptCbc(src, iv, padding);
+                    break;
+                case "ECB":
+                    resultArray = aes.DecryptEcb(src, padding);
+                    break;
+
+                case "CFB":
+                    resultArray = aes.DecryptCfb(src, iv, padding);
+                    break;
+
+            }
+            return resultArray;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static byte[] sha1(this byte[] sourceBytes)
+        {
+
+            using (SHA1 sha1Hash = SHA1.Create())
+            {
+                //从字符串到字节数组
+
+
+                byte[] hashBytes = sha1Hash.ComputeHash(sourceBytes);
+
+        
+                return hashBytes;
+
+            }
+        }
+
+
+
+        public static long CRC32(this byte[] sourceBytes)
+        {
+
+            CRC32 crc = new CRC32();
+
+            crc.Reset();
+
+            crc.Update(sourceBytes);
+
+            return crc.Value;
+
+        }
+
+
+    }
+}
